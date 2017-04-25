3ad9a97725066e23a691bc64f2c50161f740d332
Phil Barow <pbarow@gmail.com>
2017-04-25 11:45:59 -0700

    saving change to server

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ var app = express();
 var port = 8000;
 var path = require('path');
 
+app.set('port', (process.env.PORT || 8000));
+
 app.use('/', function(req, res){
   res.sendFile(path.resolve(__dirname, './public/index.html'));
 });
 
-app.listen(port);
-console.log('listening on http://localhost:', port);
+app.listen(app.get('port'), function () {
+  console.log('listening on http://localhost:', port);
+});
+
